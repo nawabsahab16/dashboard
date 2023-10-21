acf7b622fc79a3c4b38e665ccc102bcdda714007
nawabsahab16 <nawab162004@gmail.com>
2023-10-22 01:10:31 +0530

    feat: SaSS initalized

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { lazy } from "react";
+import { Suspense, lazy } from "react";
+import Loader from "./components/Loader";
 
 
 const Dashboard = lazy(() => import("./pages/Dashboard"));
@@ -12,13 +13,16 @@ const Products = lazy(() => import("./pages/Products"));
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/admin/dashboard" element={ <Dashboard /> }/>
-        <Route path="/admin/product" element={ <Products />}/>
-        <Route path="/admin/customers" element={ <Customers />}/>
-        <Route path="/admin/transaction" element={ <Transaction/>}/>
+      <Suspense fallback={<Loader />} >
+        <Routes>
+          <Route path="/admin/dashboard" element={ <Dashboard /> }/>
+          <Route path="/admin/product" element={ <Products />}/>
+          <Route path="/admin/customer" element={ <Customers />}/>
+          <Route path="/admin/transaction" element={ <Transaction/>}/>
 
-      </Routes>
+        </Routes>
+      </Suspense>
+      
     </Router>
   )
 }
